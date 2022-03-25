0072879b9f2ed56039e4d35d44ffbb31d758dafd
JOKER_7X <63082309+JOKER-7X@users.noreply.github.com>
2022-03-25 20:05:33 +0200

    Update updater.py

--- program/updater.py
+++ program/updater.py
@@ -65,7 +65,7 @@ async def update_repo(_, message: Message):
         system("git pull -f && pip3 install --no-cache-dir -r requirements.txt")
         execle(sys.executable, sys.executable, "main.py", environ)
         return
-    await msg.edit(f"bot is **up-to-date** with [main](https://github.com/USDDBOT/Shadow/tree/main)", disable_web_page_preview=True)
+    await msg.edit(f"bot is **up-to-date** with [main](https://github.com/JOKER-7X/Shadowmic/tree/main)", disable_web_page_preview=True)
 
 
 @Client.on_message(command(["restart", f"restart@{BOT_USERNAME}"]) & ~filters.edited)
